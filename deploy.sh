ee19e0ead3e40b8aa5b75cd16699c27b469f84ab
aiokr <aiokr@foxmail.com>
2018-04-05 01:36:02 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+
+cd ./public
+git init
+git config --global push.default matching
+git config --global user.email "${GitHubEMail}"
+git config --global user.name "${GitHubUser}"
+git add --all .
+git commit -m "Auto Builder of ${GitHubUser}'s Planet"
+git push --force "https://${GitHubKEY}@github.com/${GitHubUser}/planet-page.git" master
